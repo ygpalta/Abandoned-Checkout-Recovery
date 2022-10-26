2c611af64d6bc9e0b59faa7602d18ace82128f10
FIRST_NAME LAST_NAME <varun.malhotra@naukri.com>
2022-10-26 08:41:09 +0530

    smtp config defined  ...................

--- jobs/sendEmail2.js
+++ jobs/sendEmail2.js
@@ -1,43 +1,51 @@
 require("dotenv").config()
-const { workerData } = require("worker_threads");
-const { checkIsAbandoned,getList } = require('../src/models/carts.model.js');
+const { getList } = require('../src/models/carts.model.js');
 const nodeMailer = require("nodemailer");
+const { Error } = require("mongoose");
 
 async function main() {
-    console.log("Entering in Main Function");
-    // console.log(workerData.description);
-
+ 
     //Transporter configuration
     let transporter = nodeMailer.createTransport({
-        pool:true,
         host: "smtp.office365.com",
-        port: 25,
+        port: 587,
         secure:false,
         auth: {
-            user: 'estore.checkouts@outlook.com',
+            user: 'smtpserver1.2@outlook.com',
             pass: 'Checkout@Recovery',
         },
         
     });
-    console.log("transportercreated");
-
+ 
 
     transporter.verify(function (error, success) {
         if (error) {
           console.log(error);
+          throw new Error("server not setup successfully")
         } else {
-          console.log("Server is ready to take our messages");
+          console.log("Server sertup successfuly");
         }
       });
 
-    await transporter.sendMail({
-        from: "estore.checkouts@outlook.com", //SENDER
-        to: "ivmal98@gmail.com",//MULTIPLE RECEIVERS
-        subject: "Did You Forget This?", //EMAIL SUBJECT
-        text: "Hi message", //EMAIL BODY IN TEXT FORMAT
-    })
+          const list = await getList();
+    console.log("AbandonedList:", list);
+    list.forEach(async (checkout) => {
+
+        const recipient = checkout.contact_email;
+        const message = `Hi ${checkout.customer.name}, you were trying to place an order with us but failed to do so. Don't worry, You can complete your transaction by clicking on below link: \n ${process.env.URL}/checkout/${checkout.checkout_id} .`
+    
+        //Email configuration
+        await transporter.sendMail({
+            from: "smtpserver1.2@outlook.com", //SENDER
+            to: recipient, //MULTIPLE RECEIVERS
+            subject: "Did You Forget This?", //EMAIL SUBJECT
+            text: message, //EMAIL BODY IN TEXT FORMAT
+        })
+    });
 
 }
+
+
 
 main().catch(err => console.log(err));
 
