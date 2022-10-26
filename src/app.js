c9a224f01a1e6506d041b14c0b95ae0a6dfc9370
ygpalta <ygpalta@gmail.com>
2022-10-26 04:10:04 +0000

    removed imports

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
-
 const itemsRouter = require('./routes/items/items.router');
 const checkoutRouter = require('./routes/checkout/checkout.router');
 const abandonedCheckoutRouter = require('./routes/abandonedCheckout/abandonedCheckout.router');
@@ -23,8 +20,6 @@ app.use('/checkout', checkoutRouter);
 app.use('/abandonedCheckout',abandonedCheckoutRouter);
 app.use('/orders', ordersRouter);
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"))
 })
--- src/server.js
+++ src/server.js
@@ -2,10 +2,8 @@ const PORT = process.env.PORT || 8000;
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const { loadPlanetData } = require('./models/planets.model');
 const { mongoConnect } = require('./services/mongo');
 const { startBree } = require('./services/appcron');
-const { loadLaunchData } = require('./models/launches.model');
 const {main} = require('../jobs/sendEmail');
 const server = http.createServer(app);
 
@@ -17,9 +15,6 @@ async function startServer() {
     // console.log("without await");
     // main();
     await mongoConnect();
-    await loadPlanetData();
-    await loadLaunchData();
-
     startBree();
 
     server.listen(PORT, () => {
